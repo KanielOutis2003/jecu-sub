31fa332f0cdb1e96cc06e8e5d0f79a1e4b6e4a2d
Jun-del Patuasic <jundelpatuasic03@gmail.com>
2025-02-23 15:18:43 -0800

    Updated ni jecu

--- obj/Debug/net8.0/SubdivisionWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/SubdivisionWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SubdivisionWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5a90938a7ca3a3678abf0a7b1f15f00884c3736")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e19dfdb94eaa8b4887af79ceadb998871d4c64")]
 [assembly: System.Reflection.AssemblyProductAttribute("SubdivisionWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SubdivisionWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
